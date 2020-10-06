ed7869b6209fea2b4677f63c4947bca1cbc49e59
YoussefSedekPasha <45726395+YoussefPasha@users.noreply.github.com>
2020-10-06 15:33:36 +0200

    Complete get users and logout current user 🎊🎊

--- src/Components/NavigationBar/Navbar.js
+++ src/Components/NavigationBar/Navbar.js
@@ -1,17 +1,32 @@
 import React, { useContext } from "react";
 import { userContext } from "../../userContext";
 import { useHistory } from "react-router-dom";
+import "./Navbar.css";
 const Navbar = () => {
-  const { user } = useContext(userContext);
+  const { user, setUser } = useContext(userContext);
   const history = useHistory();
+  const handleClick = () => {
+    setUser(null);
+  };
   return (
     <div>
       <nav className="navbar navbar-light bg-light">
         <div className="navbar-brand">
           {user ? (
-            <button type="button" className="btn btn-primary">
-              {user}
-            </button>
+            <div>
+              <button type="button" className="btn btn-primary">
+                {user}
+              </button>
+              <div className="logout__button">
+                <button
+                  type="button"
+                  onClick={handleClick}
+                  className="btn btn-danger"
+                >
+                  LOGOUT
+                </button>
+              </div>
+            </div>
           ) : (
             history.push("/login")
           )}
--- src/Components/UsersList/UserList.js
+++ src/Components/UsersList/UserList.js
@@ -1,9 +1,10 @@
 import React, { useContext, useEffect, useState } from "react";
 import { userContext } from "../../userContext";
-
+import "./UserList.css";
 const UserList = () => {
   const { user } = useContext(userContext);
   const [users, setUsers] = useState([]);
+  const [storeUsers, setStoreUsers] = useState([]);
   useEffect(() => {
     let arr = [];
     let retrievedData = localStorage.getItem("users");
@@ -11,19 +12,42 @@ const UserList = () => {
     for (let i in jsonUsers) {
       arr.push(jsonUsers[i]);
     }
+    setStoreUsers(arr);
     arr = arr.filter(function (obj) {
       return obj.email !== user;
     });
     setUsers(arr);
   }, [user]);
+  const handleDeleteClick = (email) => {
+    let arr = [...storeUsers];
+    let arr1 = [...users];
+    arr = arr.filter(function (obj) {
+      return obj.email !== email;
+    });
+    arr1 = arr1.filter(function (obj) {
+      return obj.email !== email;
+    });
+    setStoreUsers(arr);
+    setUsers(arr1);
+    console.log(email);
+    console.log(arr);
+    localStorage.setItem("users", JSON.stringify(arr));
+  };
   return (
     <div>
       <div className="card">
         {users.map((human) => (
-          <div key={Math.random()} className="card-body">
-            <h5 className="card-title">{human.fullName}</h5>
-            <p className="card-text">{human.email}</p>
-            <div className="btn btn-danger">Delete User</div>
+          <div key={Math.random()} className="passing__human">
+            <div className="card-body">
+              <h5 className="card-title">{human.fullName}</h5>
+              <p className="card-text">{human.email}</p>
+              <button
+                className="btn btn-danger"
+                onClick={() => handleDeleteClick(human.email)}
+              >
+                Delete User
+              </button>
+            </div>
           </div>
         ))}
       </div>
